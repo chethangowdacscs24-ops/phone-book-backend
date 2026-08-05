bf9fa1accc4e2576705c8f0325ac7dd11179dd71
chethangowdacscs24-ops <chethangowdacs.cs24@bmsce.ac.in>
2026-08-05 13:20:17 +0530

    Refactor API routes to improve error handling and logging; add middleware for request body logging

--- index.js
+++ index.js
@@ -5,36 +5,53 @@ const Person = require("./modules/person");
 const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
+app.use((request , response , next)=>{
+  console.log(request.body)
+  next();
+
+})
+
 app.get("/", (req, res) => {
   res.end("hello world");
 });
-app.get("/api/persons", async (request, response) => {
+
+app.get("/api/persons", async (request, response, next) => {
   try {
     const result = await Person.find({});
     response.json(result);
-  } catch (err) {
-    response.status(500).json({ error: err.message });
+  } catch (error) {
+    next(error);
   }
 });
-app.get("/info", async (request, response) => {
+
+app.get("/info", async (request, response, next) => {
   const now = new Date();
-  const l = await Person.countDocuments({});
-  response.send(`The PHONEBOOK has info of ${l} people<br/>${now.toString()}`);
+  try{
+    const l = await Person.countDocuments({});
+    response.send(`The PHONEBOOK has info of ${l} people<br/>${now.toString()}`);
+  }
+  catch(error){
+    console.log(error);
+    next(error)
+  }
+
 });
-app.get("/api/persons/:id", async (request, response) => {
+
+app.get("/api/persons/:id", async (request, response, next) => {
   try {
     const person = await Person.findById(request.params.id);
     if (person) {
-      response.json(person);
+     return response.json(person);
     } else {
-      response.status(404).json({ error: "Person not found" });
+     return response.status(404).json({ error: "Person not found" });
     }
   } catch (err) {
-    console.log(err.message)
-    response.status(400).json({ error: "invalid id" });
+    console.log(error);
+    next(error)
   }
 });
-app.put("/api/persons/:id", async (request, response) => {
+
+app.put("/api/persons/:id", async (request, response, next) => {
   const body = request.body;
   try {
     const updatedContact = await Person.findByIdAndUpdate(
@@ -55,11 +72,12 @@ app.put("/api/persons/:id", async (request, response) => {
 
     response.json(updatedContact);
   } catch (error) {
-    console.log(err.message)
-    response.status(400).json({ error: error.message });
+    console.log(error);
+    next(error)
   }
 });
-app.post("/api/persons", (request, response) => {
+
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name) {
@@ -82,11 +100,13 @@ app.post("/api/persons", (request, response) => {
     .then((result) => {
       response.json(result);
     })
-    .catch((err) => {
-      response.status(500).json({ error: err.message });
+    .catch((error) => {
+      console.log(error);
+    next(error)
     });
 });
-app.delete("/api/persons/:id", (request, response) => {
+
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       if (result) {
@@ -95,12 +115,27 @@ app.delete("/api/persons/:id", (request, response) => {
         response.status(404).json({ error: "not found" });
       }
     })
-    .catch((err) => {
-      response.status(400).json({ error: err.message });
+    .catch((error) => {
+      console.log(error);
+    next(error)
     });
 });
+const unknownEndPoint=(request, response) =>{
+  response.status(400).json({error: "unknow endpoint plz check url"})
+  return;
+};
+app.use(unknownEndPoint);
+
+const errorHandler=(error, request, response, next)=>{
+  if(error.message=== 'CastError'){
+    return response.status(400).json({error: "malfactured id"})
+  }
+  next();
+}
+app.use(errorHandler);
 
 const port = process.env.PORT || 3001;
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
